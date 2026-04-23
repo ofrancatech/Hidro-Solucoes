65db3cadaba8675d7950358be68cdb6fdb86267d
Lucas França <ofrancatech@gmail.com>
2026-04-23 14:19:38 -0300

    Correção ortográfica

--- servicos.js
+++ servicos.js
@@ -2,7 +2,7 @@ const botaozap = document.getElementById('whatsapp')
 
 function chamarwhatsapp() {
   var telefone = "5509885759009";
-  var mensagem = "Olá, gostaria de saber mais ou contratar o serviço.";
+  var mensagem = "Olá, gostaria de saber mais e contratar o serviço.";
   var url = "https://wa.me/" + telefone + "?text=" + encodeURIComponent(mensagem);
   window.location.href = url;
 }
