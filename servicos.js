1cc0e70c24ba18f33b6fc3773099874968346038
Lucas França <ofrancatech@gmail.com>
2026-04-23 13:57:35 -0300

    Alteração do númer de whatsapp

--- servicos.js
+++ servicos.js
@@ -1,7 +1,7 @@
 const botaozap = document.getElementById('whatsapp')
 
 function chamarwhatsapp() {
-  var telefone = "5509891798219";
+  var telefone = "5509885759009";
   var mensagem = "Olá, gostaria de saber mais ou contrtar o serviço.";
   var url = "https://wa.me/" + telefone + "?text=" + encodeURIComponent(mensagem);
   window.location.href = url;
